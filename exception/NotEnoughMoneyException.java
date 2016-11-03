0f0807cc22fdc33e23cd6611617bdb9cfb498119
kelefhatul <kostyantinromanov@gmail.com>
2016-11-03 18:04:54 +0200

    Delete NotEnoughMoneyException.java

--- exception/NotEnoughMoneyException.java
+++
@@ -1,9 +0,0 @@
-package com.javarush.test.level26.lesson15.big01.exception;
-
-/**
- * Created by root on 11/3/2016.
- */
-public class NotEnoughMoneyException extends Exception {
-    public NotEnoughMoneyException() {
-    }
-}
